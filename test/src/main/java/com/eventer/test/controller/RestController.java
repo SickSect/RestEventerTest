b19fb37fe58fc78906ae18c346d0f4690528a6d2
sicksect <sleepyboy906sb@gmail.com>
2022-11-20 21:45:11 +0300

    pg

--- test/src/main/java/com/eventer/test/controller/RestController.java
+++ test/src/main/java/com/eventer/test/controller/RestController.java
@@ -1,4 +1,12 @@
 package com.eventer.test.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello, you can see me!";
+    }
 }
