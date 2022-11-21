112f3b27b19b1d9fb5d363c29688f5281bd0d53a
sicksect <sleepyboy906sb@gmail.com>
2022-11-21 18:38:52 +0300

    start write mapping methods

--- test/src/main/java/com/eventer/test/controller/TaskController.java
+++ test/src/main/java/com/eventer/test/controller/TaskController.java
@@ -3,10 +3,11 @@ package com.eventer.test.controller;
 import com.eventer.test.model.Task;
 import com.eventer.test.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TaskController {
@@ -18,4 +19,15 @@ public class TaskController {
     public Task create(@RequestBody Task task){
         return repo.save(task);
     }
+
+    @GetMapping("{/tasks}")
+    public Iterable<Task> getAll(){
+        return repo.findAll();
+    }
+
+    @GetMapping("{/tasks/{id}")
+    public Task getById(@PathVariable Long id){
+        return repo.findById(id).orElse(null);
+    }
+    
 }
